45db008421e4c9af860bed7793530d3ca74ee62f
Dawid Maroń <daw.maron@gmail.com>
2023-02-21 16:22:21 +0100

    setup project - dependencies - configuration - app plan

---
+++ src/main/java/com/marex333/inventory/configuration/AppConfiguration.java
@@ -0,0 +1,9 @@
+package com.marex333.inventory.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.marex333.inventory")
+public class AppConfiguration {
+}
